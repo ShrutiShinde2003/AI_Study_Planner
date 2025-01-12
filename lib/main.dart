f080f36b1b80a10be646d9aa4dca91a5cd58f8fc
ShrutiShinde2003 <shrutivshinde2140@gmail.com>
2025-01-12 23:41:51 +0530

    working code but doesn't create user collection in firebase database for user as well as tasks and also doesn't show the username of person that we take

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,22 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:study_planner/pages/login_page.dart';
-import 'package:study_planner/pages/register_page.dart';
-import 'package:study_planner/pages/forgot_password.dart';
-import 'package:study_planner/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:study_planner/pages/login_page.dart'; // Your login screen import here
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Initialize Firebase
-  runApp(const MyApp());
+  
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Study Planner',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false, // Disable the debug banner
-      initialRoute: '/login', // Set the initial route
-      routes: {
-        '/login': (context) => const LoginPage(),
-        '/register': (context) => const RegisterPage(),
-        '/forgot_password': (context) => const ForgotPasswordPage(),
-        '/home': (context) => const HomePage(),
-      },
+      debugShowCheckedModeBanner: false,
+      title: 'AI Study Planner',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage(), // Redirect to login page
     );
   }
 }
