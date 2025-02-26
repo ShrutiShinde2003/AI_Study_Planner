4bc2017ef0a620f179213221f50f117aca742a7d
ShrutiShinde2003 <shrutivshinde2140@gmail.com>
2025-02-26 22:32:08 +0530

    added code of gemini ai where i can chat and also upload files that generate flashcards in text form

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,42 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class ChatMessage {
+//   final String uid;
+//   final String userText;
+//   final String geminiText;
+//   final Timestamp timestamp;
+//   final int sequenceNo;
+//   final String subjectText;
+
+//   ChatMessage({
+//     required this.uid,
+//     required this.userText,
+//     required this.geminiText,
+//     required this.timestamp,
+//     required this.sequenceNo,
+//     required this.subjectText,
+//   });
+
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'uid': uid,
+//       'userText': userText,
+//       'geminiText': geminiText,
+//       'timestamp': timestamp,
+//       'sequenceNo': sequenceNo,
+//       'subjectText': subjectText,
+//     };
+//   }
+
+//   factory ChatMessage.fromMap(Map<String, dynamic> map) {
+//     return ChatMessage(
+//       uid: map['uid'],
+//       userText: map['userText'] ?? '',
+//       geminiText: map['geminiText'] ?? '',
+//       timestamp: map['timestamp'],
+//       sequenceNo: map['sequenceNo'] ?? 0,
+//       subjectText: map['subjectText'] ?? '',
+//     );
+//   }
+// }
+
